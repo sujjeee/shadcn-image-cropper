04b30af729e9e69826479a90a0312db6676179b5
Suraj <sujjeee@outlook.com>
2024-06-25 13:53:32 +0530

    update: svg -> react svg

--- src/components/svg-text.tsx
+++ src/components/svg-text.tsx
@@ -15,59 +15,59 @@ export default function SvgText() {
         fill="none"
       ></rect>
       <g
-        stroke-opacity="0.6"
-        fill-opacity="0.6"
+        strokeOpacity="0.6"
+        fillOpacity="0.6"
         transform="translate(110.26857703684436 47.29972998368248) rotate(0 110.3108709414737 14.932718157431736)"
       >
         <text
           x="0"
           y="20.92969776945643"
-          font-family="Virgil, Segoe UI Emoji"
-          font-size="23.8923490518909px"
+          fontFamily="Virgil, Segoe UI Emoji"
+          fontSize="23.8923490518909px"
           fill="#1e1e1e"
-          text-anchor="start"
+          textAnchor="start"
           style={{ whiteSpace: "pre" }}
           direction="ltr"
-          dominant-baseline="alphabetic"
+          dominantBaseline="alphabetic"
         >
           Click on the profile
         </text>
       </g>
-      <g stroke-linecap="round">
+      <g strokeLinecap="round">
         <g
-          stroke-opacity="0.6"
-          fill-opacity="0.6"
+          strokeOpacity="0.6"
+          fillOpacity="0.6"
           transform="translate(98.14074259525296 60.598433414410465) rotate(0 -44.07037129762648 -25.299216707205233)"
         >
           <path
             d="M0 0 C-9.05 -1.17, -39.6 1.44, -54.29 -7 C-68.98 -15.43, -82.5 -43.33, -88.14 -50.6"
             stroke="#1e1e1e"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             fill="none"
-            stroke-dasharray="8 9"
+            strokeDasharray="8 9"
           ></path>
         </g>
         <g
-          stroke-opacity="0.6"
-          fill-opacity="0.6"
+          strokeOpacity="0.6"
+          fillOpacity="0.6"
           transform="translate(98.14074259525296 60.598433414410465) rotate(0 -44.07037129762648 -25.299216707205233)"
         >
           <path
             d="M-68.31 -35.38 C-76.11 -41.36, -83.91 -47.35, -88.14 -50.6"
             stroke="#1e1e1e"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             fill="none"
           ></path>
         </g>
         <g
-          stroke-opacity="0.6"
-          fill-opacity="0.6"
+          strokeOpacity="0.6"
+          fillOpacity="0.6"
           transform="translate(98.14074259525296 60.598433414410465) rotate(0 -44.07037129762648 -25.299216707205233)"
         >
           <path
             d="M-82.74 -26.19 C-84.86 -35.79, -86.99 -45.39, -88.14 -50.6"
             stroke="#1e1e1e"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             fill="none"
           ></path>
         </g>
